2ce2ebec68d993670e59f2bfb953fc3e382f6d7a
Kevin <kevinyluo@gmail.com>
2020-07-25 14:32:45 -0700

    handtrack

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var handTrack = require('handtrackjs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.put('/gesture', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
