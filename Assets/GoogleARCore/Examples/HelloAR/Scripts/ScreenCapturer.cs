0c57962a66d5188e4673b3968252816ec6e1ec8f
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-07-26 03:52:34 -0700

    jpg

--- Assets/GoogleARCore/Examples/HelloAR/Scripts/ScreenCapturer.cs
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/ScreenCapturer.cs
@@ -12,85 +12,96 @@ using UnityEngine.Networking;
 public class ScreenCapturer : MonoBehaviour
 {
 
-   public Text debugText;
-   CameraImageBytes bytes;
+    public Text debugText;
+    CameraImageBytes bytes;
 
-   private static byte[] s_ImageBuffer = new byte[0];
-   private static int s_ImageBufferSize = 0;
-
-
-   // Start is called before the first frame update
-   void Start()
-   {
-      debugText.text = "False";
-
-      //TextureReaderComponent.OnImageAvailableCallback += OnImageAvailable;
-   }
-
-   // Update is called once per frame
-   void Update()
-   {
-      //bytes = Frame.CameraImage.AcquireCameraImageBytes();
-      //debugText.text = bytes.IsAvailable.ToString();
-      using (var image = Frame.CameraImage.AcquireCameraImageBytes())
-      {
-         if (image.IsAvailable)
-         {
+    private static byte[] s_ImageBuffer = new byte[0];
+    private static int s_ImageBufferSize = 0;
+    private Texture2D m_TextureToRender;
 
 
-            // Adjust buffer size if necessary.
-            int bufferSize = image.UVRowStride * image.Height;
-            if (bufferSize != s_ImageBufferSize || s_ImageBuffer.Length == 0)
+    // Start is called before the first frame update
+    void Start()
+    {
+        debugText.text = "False";
+
+        //TextureReaderComponent.OnImageAvailableCallback += OnImageAvailable;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //bytes = Frame.CameraImage.AcquireCameraImageBytes();
+        //debugText.text = bytes.IsAvailable.ToString();
+        using (var image = Frame.CameraImage.AcquireCameraImageBytes())
+        {
+            if (image.IsAvailable)
             {
-               s_ImageBufferSize = bufferSize;
-               s_ImageBuffer = new byte[bufferSize];
+
+
+                // Adjust buffer size if necessary.
+                int bufferSize = image.Width * image.Height;
+                if (bufferSize != s_ImageBufferSize || s_ImageBuffer.Length == 0)
+                {
+                    s_ImageBufferSize = bufferSize;
+                    s_ImageBuffer = new byte[bufferSize];
+                }
+
+                this.m_TextureToRender = new Texture2D(image.Width, image.Height, TextureFormat.RGBA32, false, false);
+
+                // Move raw data into managed buffer.
+                System.Runtime.InteropServices.Marshal.Copy(image.Y, s_ImageBuffer, 0, bufferSize);
+
+                m_TextureToRender.LoadRawTextureData(s_ImageBuffer);
+                m_TextureToRender.Apply();
+
+                var encodedJpg = m_TextureToRender.EncodeToJPG();
+                string base64encoded = Convert.ToBase64String(encodedJpg);
+
+                StartCoroutine(SendData(image.Width, image.Height, base64encoded));
+
+                // TODO: Send s_ImageBuffer, UVRowStride, and Height to back end
+                // debugText.text = bufferSize.ToString();
+                //List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+                //formData.Add(new MultipartFormDataSection("field1=foo&field2=bar"));
+                //formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+
+
+            }
+            else
+            {
+                return;
             }
 
-            // Move raw data into managed buffer.
-            System.Runtime.InteropServices.Marshal.Copy(image.Y, s_ImageBuffer, 0, bufferSize);
+        }
 
-            StartCoroutine(SendData(image.UVRowStride, image.Height));
+    }
 
-            // TODO: Send s_ImageBuffer, UVRowStride, and Height to back end
-            // debugText.text = bufferSize.ToString();
-            //List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-            //formData.Add(new MultipartFormDataSection("field1=foo&field2=bar"));
-            //formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+    IEnumerator SendData(int Width, int Height, string image)
+    {
+        Dictionary<string, string> form = new Dictionary<string, string>();
+        form["width"] = Width.ToString();
+        form["height"] = Height.ToString();
+        form["image"] = image;
 
+        //List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        //formData.Add(new MultipartFormDataSection("width=" + UVRowStride + "&height=" + Height + "&imageByteArray=" + Convert.ToBase64String(s_ImageBuffer)));
 
-         }
-         else
-         {
-            return;
-         }
+        UnityWebRequest www = UnityWebRequest.Post("http://35.236.108.192:5000/test", form);
 
-      }
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            debugText.text = (www.error).ToString();
+        }
+        else
+        {
+            debugText.text = www.downloadHandler.text;
+        }
+    }
 
-   }
-
-   IEnumerator SendData(int UVRowStride, int Height) {
-      Dictionary<string, string> form = new Dictionary<string, string>();
-      form["width"] = UVRowStride.ToString();
-      form["height"] = Height.ToString();
-      form["image"] = Convert.ToBase64String(s_ImageBuffer);
-
-      //List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-      //formData.Add(new MultipartFormDataSection("width=" + UVRowStride + "&height=" + Height + "&imageByteArray=" + Convert.ToBase64String(s_ImageBuffer)));
-
-      UnityWebRequest www = UnityWebRequest.Post("http://35.236.108.192:5000/test", form);
-
-      yield return www.SendWebRequest();
-      if (www.isNetworkError || www.isHttpError)
-      {
-         debugText.text = (www.error).ToString();
-      }
-      else
-      {
-         debugText.text = www.downloadHandler.text;
-      }
-   }
-
-   private void OnImageAvailable(TextureReaderApi.ImageFormatType format, int width, int height, IntPtr pixelBuffer, int bufferSize) {
-      debugText.text = "True";
-   }
+    private void OnImageAvailable(TextureReaderApi.ImageFormatType format, int width, int height, IntPtr pixelBuffer, int bufferSize)
+    {
+        debugText.text = "True";
+    }
 }
