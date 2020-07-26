21f8fd15a39357b86d0a48f75affbf238efa12d1
Nachiket Ingle <nachiket.ingle@gmail.com>
2020-07-25 21:52:50 -0700

    Started new script

--- Assets/GoogleARCore/Examples/HelloAR/Scripts/ScreenCapturer.cs
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/ScreenCapturer.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenCapturer : MonoBehaviour
 {
+
+    public Text debugText;
+    GoogleARCore.CameraImageBytes bytes;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,8 @@ public class ScreenCapturer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bytes = GoogleARCore.Frame.CameraImage.AcquireCameraImageBytes();
+        debugText.text = bytes.IsAvailable.ToString();
         
     }
 }
