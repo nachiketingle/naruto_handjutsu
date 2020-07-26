324a59c5554f9abf15b415529e86d55d794755a8
Nachiket Ingle <nachiket.ingle@gmail.com>
2020-07-25 17:40:27 -0700

    Added a few changes

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/ScreenCapturer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenCapturer : MonoBehaviour
+{
+
+    public Text debugText;
+    GoogleARCore.CameraImageBytes bytes;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bytes = GoogleARCore.Frame.CameraImage.AcquireCameraImageBytes();
+
+        debugText.text = bytes.IsAvailable.ToString();
+
+    }
+}
